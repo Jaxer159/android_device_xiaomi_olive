8b6e0e74cd929d2568e8381c2d409708b2260373
zqm64 <zqm64@outlook.com>
2021-06-07 13:05:03 +0800

    olives: CherishOS Bringup
    Please track vendor/qcom/opensource/interfaces from LOS otherwise vendor.qti.hardware.perf@2.0 would be missing.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_olives-user \
     lineage_olives-userdebug \
     lineage_olives-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/cherish_olives.mk
+
+COMMON_LUNCH_CHOICES += \
+    cherish_olives-user \
+    cherish_olives-userdebug \
+    cherish_olives-eng
