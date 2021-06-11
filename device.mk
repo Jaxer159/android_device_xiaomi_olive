3cece86d1f88a7d5efe97e44d61844f5efa88da0
Nazar Kovalenko <kn31132@gmail.com>
2021-06-11 09:45:07 +0000

    olives: Import stock camera props

--- device.mk
+++ device.mk
@@ -28,6 +28,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Properties
+include $(LOCAL_PATH)/properties.mk
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
