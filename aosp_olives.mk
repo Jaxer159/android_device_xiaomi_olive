88c45fc034304869dd14925ca9d67726fce4c55c
Nazar Kovalenko <85736144+Jaxer159@users.noreply.github.com>
2021-06-23 14:39:27 +0300

    Update aosp_olives.mk

--- aosp_olives.mk
+++ aosp_olives.mk
@@ -9,9 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Pixel Experience stuff.
+# Inherit some common PixelExperience stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
