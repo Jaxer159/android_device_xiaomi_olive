41ac1727c5ad3e10a1008c01ab062b05e3ed51ad
Nazar Kovalenko <kn31132@gmail.com>
2021-07-01 14:24:20 +0300

    Enable blur

--- properties.mk
+++ properties.mk
@@ -13,3 +13,8 @@ persist.vendor.camera.expose.aux=1
 # Charger
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.ctm.disallowed=true
+
+# Blur
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.supports_background_blur=1 \
+persist.sys.sf.disable_blurs=1
