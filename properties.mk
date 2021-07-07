f4959ff7fb31bbdab3ce003d884c12a57c851503
Nazar Kovalenko <85736144+Jaxer159@users.noreply.github.com>
2021-07-07 14:13:37 +0300

    Added power saving properties

--- properties.mk
+++ properties.mk
@@ -71,3 +71,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.zram.mark_idle_delay_mins=60 \
 ro.zram.first_wb_delay_mins=180 \
 ro.zram.periodic_wb_delay_hours=24
+
+# Power saving
+PRODUCT_PROPERTY_OVERRIDES += \
+power.saving.mode=1 \
+pm.sleep_mode=1
