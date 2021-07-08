31917efd2a7dbf54b12ade2ac7ebe6a5eb2d9255
Nazar Kovalenko <85736144+Jaxer159@users.noreply.github.com>
2021-07-08 21:38:56 +0300

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -5,18 +5,23 @@ cd ..
 cd ..
 echo "Olives files cloning script"
 echo ""
+# Clone device trees
 echo "Clone device trees..."
 git clone https://github.com/mi-msm8937/android_device_xiaomi_msm8937-common device/xiaomi/msm8937-common
 git clone https://github.com/mi-msm8937/android_device_xiaomi_sdm439-common device/xiaomi/sdm439-common
 echo ""
+# Clone kernels
 echo "Clone kernels..."
 git clone https://github.com/mi-msm8937/android_kernel_xiaomi_msm8937 kernel/xiaomi/msm8937
 git clone https://github.com/mi-msm8937/android_kernel_xiaomi_sdm439 -b a11/clean-iwlan kernel/xiaomi/sdm439
 echo ""
+# Clone vendors
 echo "Clone vendors..."
 git clone https://github.com/mi-msm8937/proprietary_vendor_xiaomi_msm8937-common vendor/xiaomi/msm8937-common
 git clone https://github.com/mi-msm8937/proprietary_vendor_xiaomi_sdm439-common vendor/xiaomi/sdm439-common
 git clone https://github.com/mi-msm8937/proprietary_vendor_xiaomi_olives vendor/xiaomi/olives
+
+# Clone camera vendor
 echo "Clone camera vendor? yes/no?"
 read choice
 choice="${choice,,}"
