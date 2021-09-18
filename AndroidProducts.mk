f237befdd983f5602ae39e443190c43fa1ae7812
Nazar Kovalenko <kn31132@gmail.com>
2021-09-18 14:53:27 +0000

    Bringup to AOSP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+#LOS
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_olives.mk
 
@@ -11,3 +12,12 @@ COMMON_LUNCH_CHOICES := \
     lineage_olives-user \
     lineage_olives-userdebug \
     lineage_olives-eng
+
+#AOSP
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_olives.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_olives-user \
+    aosp_olives-userdebug \
+    aosp_olives-eng
